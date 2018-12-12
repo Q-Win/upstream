4331a03e06f3874353cf7c4d59cbb878001a457b
Quinn Krug <quinn.krug@gmail.com>
2018-12-12 11:09:28 -0700

    added address

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,7 +4,7 @@ class Station
 
   def initialize(station_data)
     @name = station_data[:station_name]
-    # @address = station_data
+    @address = station_data[:street_address]
     @fuel_type = station_data[:fuel_type_code]
     @distance = station_data[:distance]
     @access_times = station_data[:access_days_time]
